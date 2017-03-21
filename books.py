dc93c86c1ac5327be1187c6f3e946f0384dfaf16
piscary <piscary.kaito@gmail.com>
2017-03-21 19:07:16 +0100

    Erfolgsrechnung reinskizziert und allgemeines Refactoring

--- books.py
+++ books.py
@@ -41,14 +41,16 @@ class Konto:
 		return next((konto for konto in konti if konto.code == str(self.gegenkonto_code)), None)
 
 	def get_saldo(self):
+		soll_summe = sum([buchung[0] for buchung in self.buchungen])
+		haben_summe = sum([buchung[1] for buchung in self.buchungen])
 		if self.typ == KONTO_TYP_AKTIV or self.typ == KONTO_TYP_AUFWAND:
-			return sum([buchung[0] for buchung in self.buchungen]) - sum([buchung[1] for buchung in self.buchungen])
+			return soll_summe - haben_summe
 		elif self.typ == KONTO_TYP_PASSIV or self.typ == KONTO_TYP_ERTRAG:
-			return sum([buchung[1] for buchung in self.buchungen]) - sum([buchung[0] for buchung in self.buchungen])
+			return haben_summe - soll_summe
 
 	def get_typ(self):
 		if self.get_saldo() < 0 and self.get_gegenkonto():
-			return self.get_gegenkonto().typ
+			return '-' + self.get_gegenkonto().typ
 		return self.typ
 
 	def soll_eintrag(self, betrag):
@@ -58,8 +60,12 @@ class Konto:
 		self.buchungen.append((0, betrag))
 
 	def __str__(self):
+		# TODO: Refactor this!
 		if self.get_saldo() >= 0:
 			return str(self.code + '_' + self.name[:23]).ljust(30) + str(self.get_saldo()).rjust(10, '_')
+
+	def output(self):
+		return str(self.code + '_' + self.name[:23]).ljust(30) + str(self.get_saldo()).rjust(10, '_')
 
 
 class Buchung:
@@ -79,28 +85,73 @@ class Buchung:
 
 class Bilanz:
 	def __init__(self, datum=datetime.datetime.now()):
-		aktivseite = [konto for konto in konti if konto.get_typ() == KONTO_TYP_AKTIV]
-		passivseite = [konto for konto in konti if konto.get_typ() == KONTO_TYP_PASSIV]
+		self.datum = datum
+		self.aktivseite = [konto for konto in konti if konto.get_typ() == KONTO_TYP_AKTIV]
+		self.passivseite = [konto for konto in konti if konto.get_typ() == KONTO_TYP_PASSIV]
+
+	def output(self):
 		aktiv_summe = 0
 		passiv_summe = 0
 		print('='*83)
-		print('Bilanz vom ' + dts(datum, False))
+		print('Bilanz vom ' + dts(self.datum, False))
 		print('='*83)
-		for i in range(max(len(aktivseite), len(passivseite))):
-			if i < len(aktivseite):
-				aktiven = aktivseite[i]
+		for i in range(max(len(self.aktivseite), len(self.passivseite))):
+			aktiven = ''
+			passiven = ''
+			aktiven_str = ''*40
+			passiven_str = ''*40
+			if i < len(self.aktivseite):
+				aktiven = self.aktivseite[i]
 				aktiv_summe += aktiven.get_saldo()
-			else:
-				aktiven = ''
-			if i < len(passivseite):
-				passiven = passivseite[i]
+				aktiven_str = str(aktiven.code + '_' + aktiven.name[:23]).ljust(30) + str(aktiven.get_saldo()).rjust(10, '_')
+			if i < len(self.passivseite):
+				passiven = self.passivseite[i]
 				passiv_summe += passiven.get_saldo()
-			else:
-				passiven = ''
-			if aktiven.__str__():
-				print(str(aktiven).ljust(40) + ' | ' + str(passiven).rjust(40))
+				passiven_str = str(passiven.code + '_' + passiven.name[:23]).ljust(30) + str(passiven.get_saldo()).rjust(10, '_')
+			print(aktiven_str + ' | ' + passiven_str)
 		print('-'*83)
 		print(str(aktiv_summe).ljust(40) + ' | ' + str(passiv_summe).rjust(40))
+		print('='*83)
+
+
+class Erfolgsrechnung:
+	def __init__(self):
+		self.aufwandseite = [konto for konto in konti if konto.get_typ() == KONTO_TYP_AUFWAND]
+		self.ertragseite = [konto for konto in konti if konto.get_typ() == KONTO_TYP_ERTRAG]
+
+	def update_gewinnvortrag(self):
+		aufwand = sum((konto.get_saldo() for konto in self.aufwandseite))
+		ertrag = sum((konto.get_saldo() for konto in self.ertragseite))
+		erfolg = aufwand - ertrag
+		gewinnvortrag = next((konto for konto in konti if konto.code == '2970'), None)
+		gewinnvortrag.history = []
+		if erfolg < 0:
+			gewinnvortrag.haben_eintrag(erfolg)
+		else:
+			gewinnvortrag.soll_eintrag(erfolg)
+
+	def output(self):
+		# TODO: Erfolgsrechnung verstehen, dann output planen :P
+		aufwand_summe = 0
+		ertrag_summe = 0
+		print('='*83)
+		print('Erfolgsrechnung')
+		print('='*83)
+		for i in range(max(len(self.aufwandseite), len(self.ertragseite))):
+			if i < len(self.aufwandseite):
+				aufwaendungen = self.aufwandseite[i]
+				aufwand_summe += aufwaendungen.get_saldo()
+			else:
+				aufwaendungen = ''
+			if i < len(self.ertragseite):
+				ertraege = self.ertragseite[i]
+				ertrag_summe += ertraege.get_saldo()
+			else:
+				ertraege = ''
+			if aufwaendungen.__str__():
+				print(str(aufwaendungen).ljust(40) + ' | ' + str(ertraege).rjust(40))
+		print('-'*83)
+		print(str(aufwand_summe).ljust(40) + ' | ' + str(ertrag_summe).rjust(40))
 		print('='*83)
 
 
@@ -146,3 +197,10 @@ Buchung('1510', '2000', 500)
 Bilanz()
 Buchung(2000, '1020', 200)
 Bilanz()
+Buchung(6000, 1020, 700)
+Bilanz()
+
+ef = Erfolgsrechnung()
+ef.update_gewinnvortrag()
+print(ef.output())
+print(Bilanz().output())
